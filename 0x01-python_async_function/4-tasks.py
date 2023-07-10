ee6ef7482b55bcd5b1bf1776089b5b2b8570f70a
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-07-10 18:04:01 +0100

    use a function that creates a task to simplify process

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Run coroutines concurrently"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    returns a list of result of a coroutine run n times. The
+    list is a list of tasks that were completed in a timely order
+    """
+    task_list: list = []
+    done_tasks: list = []
+
+    for _ in range(0, n):
+        task_list.append(task_wait_random(max_delay))
+
+    for res in asyncio.as_completed(task_list):
+        earliest_res = await res
+        done_tasks.append(earliest_res)
+    return done_tasks
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,9 @@
+#	Asynchronous programming in Python
+
+- Coroutines
+- Futures
+- Tasks
+- async/await syntax
+- asyncio
+
+[Link to python docs](https://docs.python.org/3/library/asyncio-task.html#coroutine)

910aa1c43294edc9b9b5efcbe0ddcb8520114d79
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-07-10 12:45:39 +0100

    measure performance through runtime of coroutines

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """ 
+    """
     returns an approximate value of the runtime of
     coroutine tasks
     """

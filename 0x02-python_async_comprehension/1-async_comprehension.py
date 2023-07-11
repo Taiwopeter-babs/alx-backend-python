197f5b13e3f2a3e2a2e48a2470c1a7e050bd6c1e
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-07-11 14:42:08 +0100

    refactor return type annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """An async generator"""
 
-from typing import Iterable
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Iterable[float]:
+async def async_comprehension() -> List[float]:
     """Generate numbers between 0 and 10"""
     result = [num async for num in async_generator()]
     return result
